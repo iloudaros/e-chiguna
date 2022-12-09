b87f80f126aea2c030aca15e78adc8782b901d1b
Ioannis Loudaros <johnloudaros@hotmail.com>
2022-12-09 22:20:43 +0200

    Update e-chiguna.sql
    Based on the changes made in e-chiguna.dbml:
    - Added comments.
    - changed " ` " with " " ".

--- Code/DB/e-chiguna.sql
+++ Code/DB/e-chiguna.sql
@@ -1,100 +1,114 @@
-CREATE TABLE `User` (
-  `username` varchar(255) PRIMARY KEY,
-  `password` varchar(255),
-  `email` varchar(255),
-  `is_manager` bit DEFAULT 0,
-  `score` integer DEFAULT 0,
-  `balance` integer DEFAULT 0
+CREATE TABLE "User" (
+  "username" varchar PRIMARY KEY,
+  "password" varchar,
+  "email" varchar,
+  "is_manager" bit DEFAULT 0,
+  "score" integer DEFAULT 0,
+  "balance" integer DEFAULT 0
 );
 
-CREATE TABLE `Store` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `address` varchar(255),
-  `longtitude` float,
-  `latitude` float
+CREATE TABLE "Store" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar,
+  "address" varchar,
+  "longtitude" float,
+  "latitude" float
 );
 
-CREATE TABLE `Discount` (
-  `id` integer PRIMARY KEY,
-  `username` varchar(255),
-  `product` integer,
-  `price` float,
-  `date` date
+CREATE TABLE "Discount" (
+  "id" integer PRIMARY KEY,
+  "username" varchar,
+  "product" integer,
+  "price" float,
+  "date" date
 );
 
-CREATE TABLE `Reaction` (
-  `username` varchar(255) PRIMARY KEY,
-  `discount` integer,
-  `liked` bit DEFAULT 0,
-  `disliked` bit DEFAULT 0,
-  `date` date
+CREATE TABLE "Reaction" (
+  "username" varchar PRIMARY KEY,
+  "discount" integer,
+  "liked" bit DEFAULT 0,
+  "disliked" bit DEFAULT 0,
+  "date" date
 );
 
-CREATE TABLE `Product` (
-  `id` integer PRIMARY KEY,
-  `name` varchar(255),
-  `category` varchar(255),
-  `subcategory` varchar(255)
+CREATE TABLE "Product" (
+  "id" integer PRIMARY KEY,
+  "name" varchar,
+  "category" varchar,
+  "subcategory" varchar
 );
 
-CREATE TABLE `Products_in_Store` (
-  `store_id` integer,
-  `product_id` integer,
-  `availability` integer
+CREATE TABLE "Products_in_Store" (
+  "store_id" integer,
+  "product_id" integer,
+  "availability" integer
 );
 
-CREATE TABLE `Price` (
-  `product_id` integer,
-  `store_id` integer,
-  `date` date,
-  `price` integer
+CREATE TABLE "Price" (
+  "product_id" integer,
+  "store_id" integer,
+  "date" date,
+  "price" integer
 );
 
-CREATE TABLE `Cart` (
-  `username` varchar(255) PRIMARY KEY,
-  `product_id` integer,
-  `quantity` integer
+CREATE TABLE "Score_History" (
+  "id" SERIAL PRIMARY KEY,
+  "username" varchar,
+  "current_score" integer,
+  "total_score" integer,
+  "date" date
 );
 
-CREATE TABLE `Score_History` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255),
-  `current_score` integer,
-  `total_score` integer,
-  `date` date
+CREATE TABLE "Token_History" (
+  "id" SERIAL PRIMARY KEY,
+  "username" varchar,
+  "tokens_given" integer,
+  "total_tokens" integer,
+  "date" date
 );
 
-CREATE TABLE `Token_History` (
-  `id` integer PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255),
-  `tokens_given` integer,
-  `total_tokens` integer,
-  `date` date
-);
+ALTER TABLE "Discount" ADD FOREIGN KEY ("username") REFERENCES "User" ("username");
 
-ALTER TABLE `User` COMMENT = 'The table where user information and their role is stored';
+ALTER TABLE "Discount" ADD FOREIGN KEY ("product") REFERENCES "Product" ("id");
 
-ALTER TABLE `Discount` ADD FOREIGN KEY (`username`) REFERENCES `User` (`username`);
+ALTER TABLE "Reaction" ADD FOREIGN KEY ("username") REFERENCES "User" ("username");
 
-ALTER TABLE `Discount` ADD FOREIGN KEY (`product`) REFERENCES `Product` (`id`);
+ALTER TABLE "Reaction" ADD FOREIGN KEY ("discount") REFERENCES "Discount" ("id");
 
-ALTER TABLE `Reaction` ADD FOREIGN KEY (`username`) REFERENCES `User` (`username`);
+ALTER TABLE "Products_in_Store" ADD FOREIGN KEY ("store_id") REFERENCES "Store" ("id");
 
-ALTER TABLE `Reaction` ADD FOREIGN KEY (`discount`) REFERENCES `Discount` (`id`);
+ALTER TABLE "Products_in_Store" ADD FOREIGN KEY ("product_id") REFERENCES "Product" ("id");
 
-ALTER TABLE `Products_in_Store` ADD FOREIGN KEY (`store_id`) REFERENCES `Store` (`id`);
+ALTER TABLE "Price" ADD FOREIGN KEY ("product_id") REFERENCES "Product" ("id");
 
-ALTER TABLE `Products_in_Store` ADD FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+ALTER TABLE "Price" ADD FOREIGN KEY ("store_id") REFERENCES "Store" ("id");
 
-ALTER TABLE `Price` ADD FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+ALTER TABLE "Score_History" ADD FOREIGN KEY ("username") REFERENCES "User" ("username");
 
-ALTER TABLE `Price` ADD FOREIGN KEY (`store_id`) REFERENCES `Store` (`id`);
+ALTER TABLE "Token_History" ADD FOREIGN KEY ("username") REFERENCES "User" ("username");
 
-ALTER TABLE `Cart` ADD FOREIGN KEY (`username`) REFERENCES `User` (`username`);
+COMMENT ON TABLE "User" IS 'The table where user information and their role is stored.';
 
-ALTER TABLE `Cart` ADD FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`);
+COMMENT ON TABLE "Store" IS 'every entry in this table represents a Store.';
 
-ALTER TABLE `Score_History` ADD FOREIGN KEY (`username`) REFERENCES `User` (`username`);
+COMMENT ON TABLE "Discount" IS 'Here we store the discounts that the users submit to e-chiguna.';
 
-ALTER TABLE `Token_History` ADD FOREIGN KEY (`username`) REFERENCES `User` (`username`);
+COMMENT ON TABLE "Reaction" IS 'Here we store the reaction of the users to a specific discount.';
+
+COMMENT ON TABLE "Product" IS 'Here we store all the products that we know. For products of a single store, check the table "Products_in_Store".';
+
+COMMENT ON TABLE "Products_in_Store" IS 'This table contains the products available in each store.';
+
+COMMENT ON TABLE "Price" IS 'This table contains the prices that are submitted from the administrator.';
+
+COMMENT ON TABLE "Score_History" IS 'This table keeps a record of the score of each user.';
+
+COMMENT ON COLUMN "Score_History"."current_score" IS 'This is the score of the month stated in the field "date".';
+
+COMMENT ON COLUMN "Score_History"."total_score" IS 'This is the sum of the score of all of the previous months, before the month stated at the field "date".';
+
+COMMENT ON TABLE "Token_History" IS 'This table keeps a record of the tokens of each user.';
+
+COMMENT ON COLUMN "Token_History"."tokens_given" IS 'These are the tokens given to the user (stated in "username") for the month stated in the field "date".';
+
+COMMENT ON COLUMN "Token_History"."total_tokens" IS 'This is the token total of the user (stated in "username") up to the previous month of the one stated in the field "date".';
